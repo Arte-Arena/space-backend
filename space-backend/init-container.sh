974410fd9858c74c99dd3bd64d8d68bb7b6a0088
Leandro Damasio <ldamasio@gmail.com>
2024-12-12 17:24:57 -0300

    test: Remove append function from script

--- space-backend/init-container.sh
+++ space-backend/init-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "DB_HOST=$SPACE_DB_HOST" >> testaaaa
+# echo "DB_HOST=$SPACE_DB_HOST" >> testaaaa
 # echo "DB_PORT=$SPACE_DB_PORT" >> ../.env
 # echo "DB_DATABASE=$SPACE_DB_DATABASE" >> ../.env
 # echo "DB_USERNAME=$SPACE_DB_USERNAME" >> ../.env
