1cb25ef40da33a669704a0a5ba00410ed68dafe7
Leandro Damasio <ldamasio@gmail.com>
2024-12-12 17:45:02 -0300

    test: change file

--- space-backend/init-container.sh
+++ space-backend/init-container.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "DB_HOST=$SPACE_DB_HOST" >> .env
-echo "DB_PORT=$SPACE_DB_PORT" >> .env
-echo "DB_DATABASE=$SPACE_DB_DATABASE" >> .env
-echo "DB_USERNAME=$SPACE_DB_USERNAME" >> .env
-echo "DB_HOST=$SPACE_DB_PASSWORD" >> .env
+# echo "DB_HOST=$SPACE_DB_HOST" >> .env
+# echo "DB_PORT=$SPACE_DB_PORT" >> .env
+# echo "DB_DATABASE=$SPACE_DB_DATABASE" >> .env
+# echo "DB_USERNAME=$SPACE_DB_USERNAME" >> .env
+# echo "DB_PASSWORD=$SPACE_DB_PASSWORD" >> .env
 
+
+echo "DB_HOST=$SPACE_DB_HOST" >> TESTAAAAA
 
 echo "Script de inicialização concluído com sucesso."
