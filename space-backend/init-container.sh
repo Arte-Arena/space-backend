fb1eb27ac018a5b403b22928149e59ae3373a82e
Leandro Damasio <ldamasio@gmail.com>
2024-12-26 13:34:19 -0300

    feat: Add frete melhor envio homolog env var API URL and Token

--- space-backend/init-container.sh
+++ space-backend/init-container.sh
@@ -6,6 +6,8 @@ echo "SPACE_DB_PORT=$SPACE_DB_PORT" >> .env
 echo "SPACE_DB_DATABASE=$SPACE_DB_DATABASE" >> .env
 echo "SPACE_DB_USERNAME=$SPACE_DB_USERNAME" >> .env
 echo "SPACE_DB_PASSWORD=$SPACE_DB_PASSWORD" >> .env
+echo "FRETE_MELHORENVIO_API_URL=$FRETE_MELHORENVIO_API_URL" >> .env
+echo "FRETE_MELHORENVIO_API_TOKEN=$FRETE_MELHORENVIO_API_TOKEN" >> .env
 
 
 echo "[arte arena security] Script de inicialização concluído com sucesso."
