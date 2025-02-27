6b417916434546fe2c08cec53a3716d4823909df
Pablo Yuri <pabloyuridev@gmail.com>
2025-02-27 14:57:07 -0300

    Add controllers

---
+++ space-backend/app/Http/Controllers/OrcamentosUniformesController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrcamentosUniformes;
+use Illuminate\Http\Request;
+
+class OrcamentosUniformesController extends Controller
+{
+    public function index()
+    {
+        return OrcamentosUniformes::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'orcamento_id' => 'required|exists:orcamentos,id',
+            'esboco' => 'required|string|max:1',
+            'quantidade_jogadores' => 'required|integer|min:1',
+            'configuracoes' => 'required|array'
+        ]);
+
+        return OrcamentosUniformes::create($request->all());
+    }
+
+    public function show(OrcamentosUniformes $orcamentosUniforme)
+    {
+        return $orcamentosUniforme;
+    }
+
+    public function update(Request $request, OrcamentosUniformes $orcamentosUniforme)
+    {
+        $request->validate([
+            'orcamento_id' => 'exists:orcamentos,id',
+            'esboco' => 'string|max:1',
+            'quantidade_jogadores' => 'integer|min:1',
+            'configuracoes' => 'array'
+        ]);
+
+        $orcamentosUniforme->update($request->all());
+        return $orcamentosUniforme;
+    }
+
+    public function destroy(OrcamentosUniformes $orcamentosUniforme)
+    {
+        $orcamentosUniforme->delete();
+        return response()->json(null, 204);
+    }
+}
