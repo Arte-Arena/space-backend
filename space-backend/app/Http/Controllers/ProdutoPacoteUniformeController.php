66b86744735e40258656d3d7b7c0fb5587f659d0
Leandro Damasio <ldamasio@gmail.com>
2025-02-27 14:04:36 -0300

    fix: Remove problematic validation on ProdutoPacoteUniformeController

--- space-backend/app/Http/Controllers/ProdutoPacoteUniformeController.php
+++ space-backend/app/Http/Controllers/ProdutoPacoteUniformeController.php
@@ -17,49 +17,23 @@ class ProdutoPacoteUniformeController extends Controller
 
         return response()->json($pacotes, 200);
     }
+
     public function upsertPacoteUniforme(Request $request, $pacote_id = null)
-    {
-        $validator = Validator::make($request->all(), [
-            'nome' => 'required|string',
-            'tipo_de_tecido_camisa' => 'required|in:Dryfit Liso,Dryfit Sport Star Liso,DryFit Camb Pro',
-            'tipo_de_tecido_calcao' => 'required|in:Dryfit Liso,Dryfit Sport Star Liso,DryFit Camb Pro',
-            'permite_gola_customizada' => 'required|boolean',
-            'tipo_gola' => 'nullable|array',
-            'permite_nome_de_jogador' => 'required|boolean',
-            'permite_escudo' => 'required|boolean',
-            'tipo_de_escudo_na_camisa' => 'nullable|array',
-            'tipo_de_escudo_no_calcao' => 'nullable|array',
-            'patrocinio_ilimitado' => 'required|boolean',
-            'patrocinio_numero_maximo' => 'nullable|integer',
-            'tamanhos_permitidos' => 'nullable|array',
-            'numero_fator_protecao_uv_camisa' => 'required|integer',
-            'numero_fator_protecao_uv_calcao' => 'required|integer',
-            'tipo_de_tecido_meiao' => 'required|in:Helanca Profissional,Helanca Profissional Premium',
-            'punho_personalizado' => 'required|boolean',
-            'etiqueta_de_produto_autentico' => 'required|boolean',
-            'logo_totem_em_patch_3d' => 'required|boolean',
-            'selo_de_produto_oficial' => 'required|boolean',
-            'selo_de_protecao_uv' => 'required|boolean',
-        ]);
+{
+    $data = $request->all();
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
+    if ($pacote_id) {
+        $produtoPacote = ProdutoPacoteUniforme::find($pacote_id);
+        if (!$produtoPacote) {
+            return response()->json(['message' => 'Pacote de Produtos não encontrado'], 404);
         }
-
-        $data = $request->all();
-
-        if ($pacote_id) {
-            $produtoPacote = ProdutoPacoteUniforme::find($pacote_id);
-            if (!$produtoPacote) {
-                return response()->json(['message' => 'Pacote de Produtos não encontrado'], 404);
-            }
-            $produtoPacote->update($data);
-        } else {
-            $produtoPacote = ProdutoPacoteUniforme::create($data);
-        }
-
-        return response()->json($produtoPacote, $pacote_id ? 200 : 201);
+        $produtoPacote->update($data);
+    } else {
+        $produtoPacote = ProdutoPacoteUniforme::create($data);
     }
+
+    return response()->json($produtoPacote, $pacote_id ? 200 : 201);
+}
 
     public function deletePacoteUniforme($pacote_id)
     {
