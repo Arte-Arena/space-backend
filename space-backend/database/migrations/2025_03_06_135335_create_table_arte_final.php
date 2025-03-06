8539f6068e14c92391ce0e0e32a263d03273e7d1
Gutavo <gustavogarcia56336@gmail.com>
2025-03-06 11:14:55 -0300

    migration create_table_pedidos_arte_final

---
+++ space-backend/database/migrations/2025_03_06_135335_create_table_arte_final.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos_arte_final', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('SET NULL');
+            $table->string('numero_pedido')->nullable();
+            $table->dateTime('data_prevista')->nullable();
+            $table->json('lista_produtos')->nullable()->default(null);
+            $table->string('pedido_produto_categoria')->nullable();
+            $table->string('pedido_material')->nullable();
+            $table->decimal('medida_linear', 10, 2)->nullable();
+            $table->text('observacoes')->nullable();
+            $table->string('rolo')->nullable();
+            $table->foreignId('designer_id')->nullable()->constrained('users')->onDelete('SET NULL');
+            $table->foreignId('pedido_status_id')->nullable()->constrained('pedidos_status')->onDelete('SET NULL');
+            $table->foreignId('pedido_tipo_id')->nullable()->constrained('pedidos_tipos')->onDelete('SET NULL');
+            $table->string('estagio')->nullable();
+            $table->string('url_trello')->nullable();
+            $table->text('situacao')->nullable();
+            $table->string('prioridade')->nullable();
+            $table->foreignId('orcamento_id')->nullable();
+            $table->foreign('orcamento_id')->references('id')->on('orcamentos')->onDelete('SET NULL');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos_arte_final');
+    }
+};
