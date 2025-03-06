f12d43382dcabf651caeefc0eff8a9c346726b67
Gutavo <gustavogarcia56336@gmail.com>
2025-03-06 11:21:16 -0300

    add: add new Model for pedido_arte_final

---
+++ space-backend/app/Models/ProdutoArteFinal.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\PedidoStatus;
+use App\Models\PedidoTipo;
+use App\Models\Orcamento;
+
+class PedidoArteFinal extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pedidos_arte_final';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'user_id', 'numero_pedido', 'data_prevista', 'lista_produtos', 'pedido_produto_categoria',
+        'pedido_material', 'medida_linear', 'observacoes', 'rolo', 'designer_id',
+        'pedido_status_id', 'pedido_tipo_id', 'estagio', 'url_trello', 'situacao',
+        'prioridade', 'orcamento_id'
+    ];
+
+    protected $casts = [
+        'lista_produtos' => 'array',
+        'data_prevista' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function designer()
+    {
+        return $this->belongsTo(User::class, 'designer_id');
+    }
+
+    public function pedidoStatus()
+    {
+        return $this->belongsTo(PedidoStatus::class, 'pedido_status_id');
+    }
+
+    public function pedidoTipo()
+    {
+        return $this->belongsTo(PedidoTipo::class, 'pedido_tipo_id');
+    }
+
+    public function orcamento()
+    {
+        return $this->belongsTo(Orcamento::class, 'orcamento_id');
+    }
+}
