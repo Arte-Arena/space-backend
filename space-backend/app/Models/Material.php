10c467b51ef4b7433d625f74d83b639cf7f2b5c1
Leandro Damasio <ldamasio@gmail.com>
2025-03-10 14:47:53 -0300

    feat: Fix materials table name

--- space-backend/app/Models/Material.php
+++ space-backend/app/Models/Material.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Material extends Model
 {
-    protected $table = 'materiais';
+    protected $table = 'materials';
     protected $fillable = ['descricao'];
     public $timestamps = false;
 }
