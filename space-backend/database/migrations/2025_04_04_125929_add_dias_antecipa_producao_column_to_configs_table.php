fcdbad4f9b4d634cef153d223635f1949adeb934
Leandro Damasio <ldamasio@gmail.com>
2025-04-04 11:07:04 -0300

    refact: Improve configs new columns deadline migration

--- space-backend/database/migrations/2025_04_04_125929_add_dias_antecipa_producao_column_to_configs_table.php
+++ space-backend/database/migrations/2025_04_04_125929_add_dias_antecipa_producao_column_to_configs_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('configs', function (Blueprint $table) {
-            $table->integer('dias_antecipa_producao')->nullable()->default(0);
+            $table->integer('dias_antecipa_producao_arte_final')->nullable()->default(0);
+            $table->integer('dias_antecipa_producao_impresao')->nullable()->default(0);
+            $table->integer('dias_antecipa_producao_confeccao')->nullable()->default(0);
+            $table->integer('dias_antecipa_producao_expedicao')->nullable()->default(0);
         });
     }
 
@@ -22,7 +25,10 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('configs', function (Blueprint $table) {
-            $table->dropColumn('dias_antecipa_producao');
+            $table->dropColumn('dias_antecipa_producao_arte_final');
+            $table->dropColumn('dias_antecipa_producao_impresao');
+            $table->dropColumn('dias_antecipa_producao_confeccao');
+            $table->dropColumn('dias_antecipa_producao_expedicao');
         });
     }
 };
