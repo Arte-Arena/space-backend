838ff4579d004ef4a3cd4044abc566d2f3ce2922
Gustavo <gustavogarcia56336@gmail.com>
2025-04-09 10:59:33 -0300

    feat: Add migrations for new arte-final tables

---
+++ space-backend/database/migrations/2025_04_09_135628_create_pedidos_arte_final_costura_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pedidos_arte_final_costura', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pedido_arte_final_id')->constrained('pedidos_arte_final')->onDelete('cascade')->unique();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pedidos_arte_final_costura');
+    }
+};
