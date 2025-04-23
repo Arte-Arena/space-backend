b65f6d23cd11a4e474deff2daf23a2f946679485
Gustavo <gustavogarcia56336@gmail.com>
2025-04-23 13:55:55 -0300

    feat: add migration and add model and relation by numero_pedido

--- space-backend/app/Models/PedidoArteFinal.php
+++ space-backend/app/Models/PedidoArteFinal.php
@@ -91,5 +91,10 @@ class PedidoArteFinal extends Model
     {
         return $this->hasOne(PedidosArteFinalConfeccaoCorteConferencia::class, 'pedido_arte_final_id');
     }
+    public function erros()
+    {
+        return $this->hasOne(Erros::class, 'numero_pedido', 'numero_pedido');
+    }
+
 
 }
