ae54caa03989aacca67a731d21eed7b37fd91279
Gustavo <gustavogarcia56336@gmail.com>
2025-05-09 09:09:41 -0300

    feat: add new fields at contas model

--- space-backend/app/Models/Conta.php
+++ space-backend/app/Models/Conta.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conta extends Model
 {
-
     protected $fillable = [
         'user_id',
         'titulo',
@@ -15,10 +14,35 @@ class Conta extends Model
         'data_vencimento',
         'status',
         'tipo',
+        'parcelas',
+        'data_pagamento',
+        'data_emissao',
+        'forma_pagamento',
+        'orcamento_staus_id',
+        'movimentacao_estoque_id',
+        'recorrencia',
+        'fixa',
+        'documento',
+        'observacoes',
+    ];
+
+    protected $casts = [
+        'parcelas' => 'array',
+        'fixa' => 'boolean',
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function orcamentoStatus()
+    {
+        return $this->belongsTo(OrcamentoStatus::class, 'orcamento_staus_id');
+    }
+
+    public function movimentacaoEstoque()
+    {
+        return $this->belongsTo(MovimentacaoEstoque::class, 'movimentacao_estoque_id');
     }
 }
